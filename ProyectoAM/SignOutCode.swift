892265fbbcfad03faaf9c4ae89c6bc7adcb86931
Fernando Medellin <fer0o@Fernandos-MacBook-Pro.local>
2017-03-22 07:04:26 -0600

    Login "terminado" firebase allan y fernando

---
+++ ProyectoAM/SignOutCode.swift
@@ -0,0 +1,50 @@
+//
+//  SignOutCode.swift
+//  ProyectoAM
+//
+//  Created by Fernando Medellin on 22/03/17.
+//  Copyright © 2017 Fernando Medellin Cuevas. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import KeychainSwift
+
+class SignOutCode: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func SignOut (_ sender: Any){
+        let firebaseAuth = FIRAuth.auth()
+        do{
+            try firebaseAuth?.signOut()
+        }
+        catch let signOutError as NSError {
+            print("No se pudo cerrar sesión...", signOutError)
+        }
+        DataService().keyChain.delete("uid")
+        //KeychainSwift().delete("uid")
+        dismiss(animated: true, completion: nil)
+    }
+}
+    //override func didReceiveMemoryWarning() {
+    //    super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    //}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+//}
